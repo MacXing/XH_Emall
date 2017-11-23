76215769c8319a9f023b2046dc050e1379db0f2c
周星 <1109497798@qq.com>
2017-11-23 12:52:16 +0800

    用户行为分析

---
+++ xhEmall/src/com/other/api/client/core/login/LoginRequest.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.other.api.client.core.login;
+
+
+/**
+ * @author baidu
+ * @date 2012-10-15
+ * @version V1.0
+ */
+public interface LoginRequest {
+
+	String getUsername();
+	
+	String getToken();
+	
+	String getUuid();
+	
+	String getFunctionName();
+	
+	Object getRequest();
+}
+
